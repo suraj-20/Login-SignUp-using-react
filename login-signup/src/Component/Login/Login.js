69aa67fde196367de2d0496776b80c6a5a0b339c
Suraj <kumarsuraj54057@gmail.com>
2023-06-10 20:51:45 +0530

    ui completed

--- login-signup/src/Component/Login/Login.js
+++ login-signup/src/Component/Login/Login.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import hero_image from "../../Assets/undraw_fingerprint_login_re_t71l.svg";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
-}
+    <>
+      <section className="register__section">
+        <div className="container">
+          <div className="left__section">
+            <img src={hero_image} alt="hero-image" />
+          </div>
 
-export default Login
+          <div className="right__section">
+            <form>
+              <h2 style={{ marginBottom: "1.5rem" }}>Login</h2>
+              <div className="input-field">
+                <input type="email" id="email" required />
+                <label for="email">Email</label>
+              </div>
+              <div className="input-field">
+                <input type="password" id="password" required />
+                <label for="password">Password</label>
+              </div>
+              <button className="primary_btn" type="submit">
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
--- login-signup/src/Component/Register/Register.js
+++ login-signup/src/Component/Register/Register.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import hero_image from "../../Assets/undraw_sign_up_n6im.svg";
+import "./Register.css";
 
 const Register = () => {
   return (
     <>
-    
-    </>
-  )
-}
+      <section className="register__section">
+        <div className="container">
+          <div className="left__section">
+            <img src={hero_image} alt="hero-image" />
+          </div>
 
-export default Register
+          <div className="right__section">
+            <form>
+              <h2 style={{ marginBottom: "1.5rem" }}>Sign Up</h2>
+              <div className="input-field">
+                <input type="text" id="name" required />
+                <label for="name">Username</label>
+              </div>
+              <div className="input-field">
+                <input type="email" id="email" required />
+                <label for="email">Email</label>
+              </div>
+              <div className="input-field">
+                <input type="password" id="password" required />
+                <label for="password">Password</label>
+              </div>
+              <button className="primary_btn" type="submit">
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Register;
